2c99853540ff013ff5be755c169e19661eb1c5f3
jjh <jjjaehoon@naver.com>
2022-07-23 00:09:47 +0900

    commit_0722_03

--- src/main/java/com/coconet/server/service/AuthService.java
+++ src/main/java/com/coconet/server/service/AuthService.java
@@ -91,7 +91,7 @@ public class AuthService {
                 refreshTokenRepository.save(saveToken);
 
                 RelssueTokenDto relssueTokenDto = new RelssueTokenDto(
-                        authentication.getName()
+                        user.getName()
                         , "true"
                         , tokenDto.getAccessToken()
                         , tokenDto.getRefreshToken()
